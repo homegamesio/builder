278b4c68dda086f3ae4f84ae12c2a7ad5bebe855
Joseph Garcia <prosif@gmail.com>
2021-01-06 21:21:44 -0700

    wip

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const https = require('https');
 const http = require('http');
 const fs = require('fs');
+const unzipper = require('unzipper');
 const config = require('./config');
 const path = require('path');
 const aws = require('aws-sdk');
@@ -22,9 +23,21 @@ const getReqBody = (req, cb) => {
 };
 
 const getBuildHistory = (limit = 10) => {
-	const dynamoClient = new aws.DynamoDB();
-	console.log(dynamoClient);
-	dynamoClient.listTables({}, (err, data) => {
+	const dynamoClient = new aws.DynamoDB({region: config.AWS_REGION});
+	const params = {
+		TableName: config.TABLE_NAME,
+		Limit: limit,
+		KeyConditionExpression: 'date_started <= :now',
+		ScanIndexForward: false, // sort descending
+		ExpressionAttributeValues: {
+			':now': {"N": "" + Date.now()}
+		}
+	};
+
+	console.log(params);
+
+	dynamoClient.query(params, (err, data) => {
+		console.log(err);
 		console.log(data);
 		return 'hello\nworld';
 	});
@@ -47,9 +60,77 @@ const server = https.createServer(options, (req, res) => {
 
 });
 
+const getLatestCommitHash = () => new Promise((resolve, reject) => {
+	https.get({
+		hostname: 'api.github.com',
+		path: '/repos/homegamesio/homegames/commits',
+		headers: {
+			'User-Agent': 'HomegamesBuilder/0.1.0'
+		}
+	},(_response) => {
+		getReqBody(_response, (response) => {
+			const data = JSON.parse(response);
+			resolve(data[0].sha);
+		});
+	});
+});
+
+const downloadHomegames = () => new Promise((resolve, reject) => {
+    // todo: uuid
+    const dir = `/home/ubuntu/tmp/${Date.now()}`;
+    const file = fs.createWriteStream(dir + '.zip');
+	const zlib = require('zlib');
+    https.get("https://codeload.github.com/homegamesio/homegames/zip/master", (_response) => {
+	    _response.pipe(unzipper.Extract({ path: dir }));
+	    resolve(dir);
+	});
+});
+
+// todo: picodeg.io (docker)
+// also hg web
+
 const checkLatestVersion = () => {
-    console.log("need to check latest version");
+	getCurrentBuildHash().then(currentHash => {
+		console.log('current hash');
+		console.log(currentHash);
+		getLatestCommitHash().then(latestHash => {
+			console.log('latest hash');
+			console.log(latestHash);
+			if (latestHash != currentHash) {
+				console.log('gotta build');
+				downloadHomegames().then(path => {
+					console.log(path);
+					const {exec} = require('child_process');
+					exec('npm install --prefix ' + path + '/homegames-master', (err, stdout, stderr) => {
+						console.log('yeah i did that');
+						exec('pkg ' + path + '/homegames-master --targets node13-linux-x64,node13-macos-x64,node13-win-x64', (err, stdout, stderr) => {
+							console.log('what happened');
+							console.log(stdout);
+							console.log(stderr);
+						});
+					});
+				});
+			}
+		});
+	});
 };
+
+const getCurrentBuildHash = () => new Promise((resolve, reject) => {
+	const dynamoClient = new aws.DynamoDB({region: config.AWS_REGION});
+	const params = {
+		TableName: config.TABLE_NAME,
+		Key: {
+			'status': {
+				S: 'current'
+			}
+		}
+	};
+
+	dynamoClient.getItem(params, (err, data) => {
+		resolve(data.Item.commit_hash.S);
+	});
+
+});
 
 setInterval(checkLatestVersion, 10 * 1000)
 
