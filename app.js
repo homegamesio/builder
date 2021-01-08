0060c27e1c2a820d755061537869ae8a70197d58
Joseph Garcia <prosif@gmail.com>
2021-01-07 21:11:16 -0700

    wip

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const unzipper = require('unzipper');
 const config = require('./config');
 const path = require('path');
 const aws = require('aws-sdk');
+const { spawn } = require('child_process');
 
 const options = {
   key: fs.readFileSync(config.SSL_KEY_PATH),
@@ -23,24 +24,8 @@ const getReqBody = (req, cb) => {
 };
 
 const getBuildHistory = (limit = 10) => {
-	const dynamoClient = new aws.DynamoDB({region: config.AWS_REGION});
-	const params = {
-		TableName: config.TABLE_NAME,
-		Limit: limit,
-		KeyConditionExpression: 'date_started <= :now',
-		ScanIndexForward: false, // sort descending
-		ExpressionAttributeValues: {
-			':now': {"N": "" + Date.now()}
-		}
-	};
-
-	console.log(params);
-
-	dynamoClient.query(params, (err, data) => {
-		console.log(err);
-		console.log(data);
-		return 'hello\nworld';
-	});
+	//todo
+	return [];
 };
 
 const server = https.createServer(options, (req, res) => {
@@ -49,9 +34,11 @@ const server = https.createServer(options, (req, res) => {
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end('ok');
         } else if (req.url === '/') {
-	    const buildHistory = getBuildHistory(10);
-	    res.writeHead(200, {'Content-Type': 'text/plain'});
-	    res.end('My last 10 builds:\n' + buildHistory);
+		getCurrentBuildInfo().then(currentBuildInfo => {
+			console.log(currentBuildInfo);
+		    res.writeHead(200, {'Content-Type': 'text/plain'});
+		    res.end(`Built ${currentBuildInfo.commitHash} at ${currentBuildInfo.dateBuilt}`);
+		});
 	}
     } else {
 	    res.writeHead(404);
@@ -89,33 +76,153 @@ const downloadHomegames = () => new Promise((resolve, reject) => {
 // todo: picodeg.io (docker)
 // also hg web
 
-const checkLatestVersion = () => {
-	getCurrentBuildHash().then(currentHash => {
-		console.log('current hash');
-		console.log(currentHash);
+const installNPMDependencies = (projectDir) => new Promise((resolve, reject) => {
+	const install = spawn('npm', ['install', '--prefix', projectDir]);
+
+	install.stdout.on('data', (data) => {
+		console.log("DATTTTA");
+		console.log(data.toString());	
+	});
+
+	install.stderr.on('data', (data) => {
+		console.log('errrrr');
+		console.log(data.toString());
+	});
+
+	install.on('close', (code) => {
+		console.log("cloooosed");
+		console.log(code);
+		resolve();
+	});
+//						exec('pkg ' + path + '/homegames-master --targets node13-linux-x64,node13-macos-x64,node13-win-x64 --out-path ' + outPath, (err, stdout, stderr) => {
+//							console.log('what happened');
+//							console.log(stdout);
+//							console.log(stderr);
+//						});
+//					});
+
+});
+
+const buildPkg = (packagePath, outPath) => new Promise((resolve, reject) => {
+	const targets = [
+		'node13-linux-x64',
+		'node13-macos-x64',
+		'node13-win-x64'
+	];
+
+	let targetString = '';
+
+	targets.forEach(t => targetString += t + ',');
+
+	targetString = targetString.slice(0, -1);
+	console.log("target string");
+	console.log(targetString);
+
+	const build = spawn('pkg', [packagePath, '--targets', targetString, '--out-path', outPath]);
+
+	build.stdout.on('data', (data) => {
+		console.log("build DATTTTA");
+		console.log(data.toString());	
+	});
+
+	build.stderr.on('data', (data) => {
+		console.log('build errrrr');
+		console.log(data.toString());
+	});
+
+	build.on('close', (code) => {
+		console.log("build cloooosed");
+		console.log(code);
+		resolve();
+	});
+
+});
+
+const updateCurrentBuildInfo = (commitHash) => new Promise((resolve, reject) => {
+	const dynamoClient = new aws.DynamoDB({region: config.AWS_REGION});
+
+	const params = {
+		TableName: config.TABLE_NAME,
+		Item: {
+			'status': {
+				'S': 'current'
+			},
+			'commit_hash': {
+				'S': commitHash
+			},
+			'date_built': {
+				'N': '' + Date.now()
+			}
+		}
+	};
+
+	dynamoClient.putItem(params, (err, data) => {
+		console.log("DSFNDGF PUT");
+		console.log(err);
+		console.log(data);
+		resolve(data);
+	});
+});
+
+const uploadBuild = () => new Promise((resolve, reject) => {
+	const linuxPath = config.BUILD_PATH + '/homegames-linux';
+	const windowsPath = config.BUILD_PATH + '/homegames-win.exe';
+	const macPath = config.BUILD_PATH + '/homegames-macos';
+
+	fs.chmodSync(linuxPath, '555');
+	fs.chmodSync(windowsPath, '555');
+	fs.chmodSync(macPath, '555');
+
+	const options = { partSize: 10 * 1024 * 1024, queueSize: 1 };
+
+	const s3Client = new aws.S3();
+
+	const linuxReadStream = fs.createReadStream(linuxPath);
+	const windowsReadStream = fs.createReadStream(windowsPath);
+	const macReadStream = fs.createReadStream(macPath);
+
+	const linuxParams = { Bucket: config.S3_BUCKET, Key: config.S3_BUILD_PREFIX + '/homegames-linux', Body: linuxReadStream, ACL: 'public-read', ContentType: 'application/x-binary' };
+	const windowsParams = { Bucket: config.S3_BUCKET, Key: config.S3_BUILD_PREFIX + '/homegames-win.exe', Body: windowsReadStream, ACL: 'public-read', ContentType: 'application/x-binary' };
+	const macParams = { Bucket: config.S3_BUCKET, Key: config.S3_BUILD_PREFIX + '/homegames-macos', Body: macReadStream, ACL: 'public-read', ContentType: 'application/x-binary' };
+
+	s3Client.upload(linuxParams, (err, data) => {
+		s3Client.upload(windowsParams, (err, data) => {
+			s3Client.upload(macParams, (err, data) => {
+				console.log("uploaded all 3");
+			});
+		});
+	});
+});
+
+uploadBuild();
+console.log('fdsfsdfdsf');
+
+const workflow = () => {
+	getCurrentBuildInfo().then(currentInfo => {
+		const currentHash = currentInfo.commitHash;
 		getLatestCommitHash().then(latestHash => {
-			console.log('latest hash');
-			console.log(latestHash);
 			if (latestHash != currentHash) {
-				console.log('gotta build');
 				downloadHomegames().then(path => {
-					console.log(path);
-					const {exec} = require('child_process');
-					exec('npm install --prefix ' + path + '/homegames-master', (err, stdout, stderr) => {
-						console.log('yeah i did that');
-						exec('pkg ' + path + '/homegames-master --targets node13-linux-x64,node13-macos-x64,node13-win-x64', (err, stdout, stderr) => {
-							console.log('what happened');
-							console.log(stdout);
-							console.log(stderr);
+					installNPMDependencies(path + '/homegames-master').then(() => {
+						console.log('installed dependencies');
+						buildPkg(path + '/homegames-master', config.BUILD_PATH).then(() => {
+							uploadBuild().then(() => {
+								updateCurrentBuildInfo(latestHash).then((buildInfo) => {
+									console.log('built');
+									console.log(buildInfo);
+								});
+							});
 						});
 					});
 				});
+			} else {
+				console.log('still good');
 			}
 		});
 	});
 };
 
-const getCurrentBuildHash = () => new Promise((resolve, reject) => {
+const getCurrentBuildInfo = () => new Promise((resolve, reject) => {
 	const dynamoClient = new aws.DynamoDB({region: config.AWS_REGION});
 	const params = {
 		TableName: config.TABLE_NAME,
@@ -127,12 +234,16 @@ const getCurrentBuildHash = () => new Promise((resolve, reject) => {
 	};
 
 	dynamoClient.getItem(params, (err, data) => {
-		resolve(data.Item.commit_hash.S);
+		resolve({
+			commitHash: data.Item.commit_hash.S,
+			dateBuilt: new Date(data.Item.date_built.N)
+		});
 	});
 
 });
 
-setInterval(checkLatestVersion, 10 * 1000)
+workflow();
+//setInterval(workflow, 10 * 1000)
 
 const HTTPS_PORT = 443;
 
